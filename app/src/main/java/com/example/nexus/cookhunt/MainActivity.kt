62297498f832e185392f3ea5584a2cd0630e8414
Whatevaaar <emilianohidalgo314@gmail.com>
2019-03-28 23:58:50 -0600

    Updt

--- app/src/main/java/com/example/nexus/cookhunt/MainActivity.kt
+++ app/src/main/java/com/example/nexus/cookhunt/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.nexus.cookhunt
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Scraper().execute()
-        main()
+        Scraper(this).execute()
     }
 
-    fun main(){
-        lateinit var listView: ListView
+    fun updateList(lista: ArrayList<String>){
+        val adapter = ArrayAdapter(this, R.layout.listview_item, lista)
+        recipe_list_view.adapter = adapter
 
     }
 }
--- app/src/main/java/com/example/nexus/cookhunt/Scraper.kt
+++ app/src/main/java/com/example/nexus/cookhunt/Scraper.kt
@@ -2,15 +2,17 @@ package com.example.nexus.cookhunt
 
 import android.os.AsyncTask
 import android.widget.ArrayAdapter
-import android.widget.ListView
-import com.example.nexus.cookhunt.R.id.recipe_list_view
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import android.app.Activity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ref.WeakReference
 
-
-class Scraper() : AsyncTask<Void, Void, Void>() {
+class Scraper (act: Activity) : AsyncTask<Void, Void, Void>() {
     lateinit var pagina: Document
     lateinit var recetas: List<String>
+    val actividad= WeakReference(act)
+
     override fun doInBackground(vararg params: Void?): Void? {
         pagina = Jsoup.connect("https://www.cocinacaserayfacil.net/todas-las-recetas/").get()
         recetas = pagina.select("a") .map { it.text() }. filter{it.contains("–")}
@@ -19,7 +21,8 @@ class Scraper() : AsyncTask<Void, Void, Void>() {
     }
 
     override fun onPostExecute(result: Void?) {
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recetas)
+        val lista = ArrayList(recetas)
+        (actividad.get() as MainActivity).updateList(lista)
         return
     }
 }
